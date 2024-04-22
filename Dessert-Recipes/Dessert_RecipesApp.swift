5c6021a292fc11ad713cfe687c018f8e7f5412f6
Mary Stirling Brown <marystirlingbrown@gmail.com>
2024-04-22 15:03:02 -0500

    initialize app project

---
+++ Dessert-Recipes/Dessert_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dessert_RecipesApp.swift
+//  Dessert-Recipes
+//
+//  Created by Mary Stirling Brown on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Dessert_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
