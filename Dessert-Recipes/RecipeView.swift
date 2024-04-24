e21e4742fcd51d305e4a3d86e069348a8cfa5c45
Mary Stirling Brown <marystirlingbrown@gmail.com>
2024-04-23 21:58:06 -0500

    instructions showing correctly

--- Dessert-Recipes/RecipeView.swift
+++ Dessert-Recipes/RecipeView.swift
@@ -1,12 +1,4 @@
-//
-//  RecipeView.swift
-//  Dessert-Recipes
-//
-//  Created by Mary Stirling Brown on 4/22/24.
-//
-
 import SwiftUI
-
 
 struct RecipeView: View {
     
@@ -15,36 +7,52 @@ struct RecipeView: View {
     var dessert: Dessert
     
     var body: some View {
-        VStack(alignment: .center){
-            Text(dessert.strMeal)
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .lineLimit(1)
-            Spacer()
-            AsyncImage(url: URL(string: dessert.strMealThumb)) { phase in
-                switch phase {
-                case .empty:
-                    ProgressView()
-                case .success(let image):
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(maxWidth: .infinity)
-                case .failure:
-                    Image(systemName: "photo")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(maxWidth: .infinity)
+        ScrollView {
+            VStack(alignment: .center){
+                Text(dessert.strMeal)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .lineLimit(2)
+                
+                Spacer()
+                
+                AsyncImage(url: URL(string: dessert.strMealThumb)) { phase in
+                    switch phase {
+                    case .empty:
+                        ProgressView()
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: 350)
+                    case .failure:
+                        Image(systemName: "photo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: .infinity)
+                    
+                    }
                 }
+                Spacer()
+                Spacer()
+                Spacer()
+                Text("Instructions")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .underline()
+                
+                Text(recipeVM.recipeArray.first?.strInstructions ?? "none")
+                    .font(.body)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                
+                
             }
-
-            
-            Spacer()
-            Text(recipeVM.recipeArray.first?.strInstructions ?? "none").font(.largeTitle).bold()
-            
-        }.task {
-            recipeVM.idNum = dessert.idMeal
-            await recipeVM.getData()
+            .task {
+                recipeVM.idNum = dessert.idMeal
+                await recipeVM.getData()
+            }
         }
     }
 }
